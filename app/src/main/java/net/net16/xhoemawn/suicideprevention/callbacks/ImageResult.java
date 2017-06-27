4de6f5ce28af66f935030758e0f360bfbe5e1ef4
xhoemawn12 <khadkasuman20@gmail.com>
2017-06-27 09:16:18 +0545

    Added New Icons

---
+++ app/src/main/java/net/net16/xhoemawn/suicideprevention/callbacks/ImageResult.java
@@ -0,0 +1,12 @@
+package net.net16.xhoemawn.suicideprevention.callbacks;
+
+import android.content.Intent;
+
+/**
+ * Created by khadk on 6/26/2017.
+ */
+
+public interface ImageResult {
+    public void resultStatus(Boolean boo, Intent intent,Integer type);
+
+}
---
+++ app/src/main/java/net/net16/xhoemawn/suicideprevention/tools/UserType.java
@@ -0,0 +1,11 @@
+package net.net16.xhoemawn.suicideprevention.tools;
+
+/**
+ * Created by khadk on 6/23/2017.
+ */
+
+public class UserType {
+    public static int HELPER = 0;
+    public static int ADMIN = 1;
+    public static int VICTIM = 2;
+}
